bbfcb04f73ce40229d5eda8c5e4d9e499c58bd3a
Shweta Dutta <shwetadutta100@gmail.com>
2018-02-27 21:53:08 -0800

    completed outline

--- outline.c
+++ outline.c
@@ -35,7 +35,6 @@ void no_rst_fault_loop(void);
 
 void wait_loop(void)
 {
-    int no_transition_req = 1; // set to 0 if state change needed
 	while(no_transition_req) // after setting to 0, update SmState and break
 	{
 	    // check for CAN
@@ -57,13 +56,12 @@ void wait_loop(void)
 
 void drive_loop(void)
 {
-	int no_transition_req = 1; // set to 0 if state change needed
 	while(no_transition_req) // after setting to 0, update SmState and break
 	{
 	    // check for CAN        
 
-	    // ACCEPT: PEDAL_BRAKE if (value changes, or above threshold - ask IO team) -> START_BRAKE, call brake_light_on(), send CAN CAR_STATE update
-	    //         PEDAL_ACEL if (value changes, or above threshold - ask IO team) -> do not change state, 
+	    // ACCEPT: PEDAL_BRAKE if (value changes, or above threshold - ask IO team) -> call brake_light_on(), send CAN message to MC with new value
+	    //         PEDAL_ACEL if (value changes, or above threshold - ask IO team) -> do not change state, send CAN message to MC with new value 
 	    //         PWR_DRAW if > 80 kW -> do not change state, send CAN power limit request (ask MC team what they want)
 	    //         BPPC -> RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BPPC fault? - change constants)
 	    //         BMS_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BMS fault?)
@@ -73,5 +71,55 @@ void drive_loop(void)
 	    //         BSE_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an BSE fault?)
 
 	    // do not check GPIO start button, not relevant in DRIVE      
+	}
+}
+
+void start_brake_loop(void)
+{
+	while(no_transition_req) // after setting to 0, update SmState and break
+	{
+	    // check for CAN      
+
+	    // ACCEPT: PEDAL_BRAKE if (value changes, or above threshold - ask IO team) -> do not change state, send CAN message to MC with new value
+	    //         PEDAL_ACEL if (value changes, or above threshold - ask IO team) -> WAIT, send CAN message to MC with new value 
+	    //         PWR_DRAW if > 80 kW -> do not change state, send CAN power limit request (ask MC team what they want)
+	    //         BPPC -> RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BPPC fault? - change constants)
+	    //         BMS_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BMS fault?)
+	    //         IMD_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an IMD fault?)
+	    //         BSPD_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BSPD fault?)
+	    //         APPS_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an APPS fault?)
+	    //         BSE_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an BSE fault?)
+
+	    // check GPIO for start button pressed
+	    //         if value read from pin = active (aka start button is pressed) -> DRIVE, call RTDS_on() send CAN CAR_STATE update
+	    //         else ignore  
+	}
+}
+
+void rst_fault_loop(void)
+{
+	while(no_transition_req) // after setting to 0, update SmState and break
+	{
+	    // check for CAN   
+	    // IGNORE: PEDAL_BRAKE
+	    //         PEDAL_ACEL
+	    //         PWR_DRAW
+	    //         BPPC   
+
+	    // ACCEPT: BMS_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BMS fault?)
+	    //         IMD_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an IMD fault?)
+	    //         BSPD_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is a BSPD fault?)
+	    //         APPS_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an APPS fault?)
+	    //         BSE_NO_RST -> NO_RST_FAULT, send CAN CAR_STATE update (w/ indication that this is an BSE fault?)
+
+	    // do not check GPIO start button, not relevant in RST_FAULT    
+	}
+}
+
+void no_rst_fault_loop(void)
+{
+	while(no_transition_req) // after setting to 0, update SmState and break
+	{ 
+	    // IGNORE: literally everything, can't escape this state, it's non-resettable!
 	}
 }
