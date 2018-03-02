d7df2afe773e99916e502ee6f21373c592d0ef59
tar-singh <35475658+tar-singh@users.noreply.github.com>
2018-03-01 21:06:18 -0800

    second commit

--- functions.c
+++ functions.c
@@ -1,5 +1,6 @@
 // Functions for state transitions. Takes in CAN message and current state. Function names in form of SmState_Event.
 void WAIT_PEDAL_BRAKE();
+void WAIT_PEDAL_BRAKE_RELEASED();
 void WAIT_BPPC_RST();
 void WAIT_IMD_NO_RST();
 void WAIT_BSPD_NO_RST();
@@ -9,6 +10,7 @@ void WAIT_BMS_NO_RST();
 
 void DRIVE_PEDAL_ACEL();
 void DRIVE_PEDAL_BRAKE();
+void DRIVE_PEDAL_BRAKE_RELEASED();
 void DRIVE_PWR_80();
 void DRIVE_BPPC_RST();
 void DRIVE_IMD_NO_RST();
@@ -20,6 +22,7 @@ void DRIVE_BMS_NO_RST();
 void START_BRAKE_START();
 void START_BRAKE_PEDAL_ACEL();
 void START_BRAKE_PEDAL_BRAKE();
+void START_BRAKE_PEDAL_BRAKE_RELEASED();
 void START_BRAKE_PWR_80();
 void START_BRAKE_BPPC_RST();
 void START_BRAKE_IMD_NO_RST();
@@ -43,6 +46,10 @@ void WAIT_PEDAL_BRAKE()
     // change GPIO pin for brake_light_on()
     // send START_BRAKE_CAN
     // send_new_brake_CAN() (altho prob not)
+}
+void WAIT_PEDAL_BRAKE_RELEASED()
+{
+    // change GPIO pin for brake_ligth_off()
 }
 void WAIT_BPPC_RST()
 {
@@ -78,6 +85,10 @@ void DRIVE_PEDAL_BRAKE()
 {
     // change GPIO pin for brake_light_on()
     // send_new_brake_CAN() (altho prob not)
+}
+void DRIVE_PEDAL_BRAKE_RELEASED()
+{
+    // change GPIO pin for brake_light_off()
 }
 void DRIVE_PWR_80()
 {
@@ -120,6 +131,11 @@ void START_BRAKE_PEDAL_ACEL()
 void START_BRAKE_PEDAL_BRAKE()
 {
     // change GPIO pin for brake_light_on()
+}
+void START_BRAKE_PEDAL_BRAKE_RELEASED()
+{
+    // change GPIO pin for brake_light_off()
+    // send WAIT_CAN
 }
 void START_BRAKE_PWR_80()
 {
