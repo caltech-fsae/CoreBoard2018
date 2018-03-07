baf7e6ec7bc985a2874af7160727a75b3120d322
tar-singh <35475658+tar-singh@users.noreply.github.com>
2018-03-06 21:33:28 -0800

    filled out some functions

--- CoreBoardRev1/Src/sm_functions.c
+++ CoreBoardRev1/Src/sm_functions.c
@@ -15,6 +15,7 @@ void RTDS()
 // Functions for state transitions. Takes in CAN message and current state. Function names in form of SmState_Event.
 void PEDAL_ACEL()
 {
+
     // occurs when acel value changed enough to care
 }
 void PEDAL_BRAKE_PUSHED()
@@ -31,56 +32,63 @@ void PWR_80()
 }
 void BPPC_RST()
 {
-    // send 0 torque command
-	// send entering reset fault state from bppc
+	send_FLT_CAN(MID_HANDLE_RST_FLT, BPPC_FLT);
 }
 void IMD_NO_RST()
 {
-	// send 0 torque command
-    // send entering reset fault state from imd
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, IMD_FLT);
 }
 void BSPD_NO_RST()
 {
-	// send 0 torque command
-	// send entering reset fault state from BSPD
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, BSPD_FLT);
 }
 void APPS_NO_RST()
 {
-    //
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, APPS_FLT);
 }
 void BSE_NO_RST()
 {
-    //
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, BSE_FLT);
+
 }
 void BMS_NO_RST()
 {
-    //
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, BMS_FLT);
 }
 
 void RST()
 {
-	//
+	send_FLT_CAN(MID_HAND_RST_FLT, GENERIC_RST);
 }
 void NO_RST()
 {
-	//
+	send_FLT_CAN(MID_HANDLE_NO_RST_FLT, GENERIC_NO_RST);
 }
 
-void send_CAN()
+void send_CAN(uint16_t MID, uint16_t message)
 {
 	// helper function for all the other functions, also for transitions that do nothing but change state
 	// will send the car state over can
 	// transmit CAN message idiom
-		  /*
-		  msg = 0x00000 // update with appropriate msg, construct from common.h/c
-		  CAN_short_msg(&can_msg, create_ID(0,0), msg);
-		  CAN_queue_transmit(&can_msg);
-		  */
+	can_msg_t can_msg;
+	CAN_short_msg(&can_msg, create_ID(BID_CORE, MID), message);
+	CAN_queue_transmit(&can_msg);
+}
+
+void send_FLT_CAN(uint16_t MID, uint16_t message)
+{
+	// helper function to send CAN specifically for faults
+	send_CAN(BID, MID, message);
+
+	can_msg_t can_msg;
+	CAN_short_msg(&can_msg, create_ID(BID_CORE, MID_TORQUE_COMMAND), 0);
+	CAN_queue_transmit(&can_msg);
+
 }
 
 void do_nothing()
 {
-    // return
+    // do nothing
 }
 
 void BRAKE_LIGHT_ON()
