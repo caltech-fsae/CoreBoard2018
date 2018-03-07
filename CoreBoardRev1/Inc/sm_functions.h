0fc65b31807105b3f547f189d1596616f229bf40
tar-singh <35475658+tar-singh@users.noreply.github.com>
2018-03-06 21:33:04 -0800

    filled out flt functions

--- CoreBoardRev1/Inc/sm_functions.h
+++ CoreBoardRev1/Inc/sm_functions.h
@@ -8,6 +8,24 @@
 #ifndef SM_FUNCTIONS_H_
 #define SM_FUNCTIONS_H_
 
+// Bit 5: generic fault (YES/NO)
+// Bit 4: IMD     fault (YES/NO)
+// Bit 3: BSPD    fault (YES/NO)
+// Bit 2: APPS    fault (YES/NO)
+// Bit 1: BSE     fault (YES/NO)
+// Bit 0: BMS     fault (YES/NO)
+
+static const uint16_t GENERIC_NO_RST  = 0b100000;
+static const uint16_t IMD_FLT         = 0b10000;
+static const uint16_t BSPD_FLT        = 0b1000;
+static const uint16_t APPS_FLT        = 0b100;
+static const uint16_t BSE_FLT         = 0b10;
+static const uint16_t BMS_FLT         = 0b1;
+
+// Bit 1: generic fault (YES/NO)
+// Bit 0: BPPC    fault (YES/NO)
+static const uint16_t GENERIC_RST     = 0b10;
+static const uint16_t BPPC_FLT        = 0b1;
 
 void RTDS();
 void PEDAL_ACEL();
@@ -24,7 +42,8 @@ void RST();
 void NO_RST();
 
 //above functions will use send_CAN
-void send_CAN();
+void send_CAN(uint16_t MID, uint16_t message);
+void send_FLT_CAN(uint16_t MID, uint16_t message);
 //below functions will not use send_CAN
 
 void do_nothing();
